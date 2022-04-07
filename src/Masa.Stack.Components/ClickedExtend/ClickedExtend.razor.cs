53d6fb5ceba5959834f2720b1eea93b382be88a4
Mayue <mayue@lonsid.cn>
2022-04-07 11:16:51 +0800

    feat:add component for auth

---
+++ src/Masa.Stack.Components/ClickedExtend/ClickedExtend.razor.cs
@@ -0,0 +1,34 @@
+﻿namespace Masa.Stack.Components;
+
+public partial class ClickedExtend
+{
+    string displayNone = "display:none !important;", displayFlex = "display:flex;";
+
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+
+    [Parameter]
+    public RenderFragment? ExtendContent { get; set; }
+
+    public bool IsExpanded { get; set; }
+
+    [Parameter]
+    public EventCallback<KeyboardEventArgs> OnKeyDown { get; set; }
+
+    [Parameter]
+    public EventCallback OnBlur { get; set; }
+
+    protected override void OnParametersSet()
+    {
+        base.OnParametersSet();
+    }
+
+    private async Task OnBlurHandler()
+    {
+        IsExpanded = false;
+        if (OnBlur.HasDelegate)
+        {
+            await OnBlur.InvokeAsync();
+        }
+    }
+}
