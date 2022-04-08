f8f6c8c21c4970bd2c492db32b382e8fa66ff0ed
Mayue <mayue@lonsid.cn>
2022-04-08 18:00:14 +0800

    feat:add component for auth

--- src/Masa.Stack.Components/_Imports.cs
+++ src/Masa.Stack.Components/_Imports.cs
@@ -2,7 +2,7 @@
 global using BlazorComponent.I18n;
 global using Masa.Blazor;
 global using Masa.Stack.Components.Configs;
-global using Microsoft.AspNetCore.Components;
-global using Microsoft.Extensions.DependencyInjection;
 global using Masa.Stack.Components.Models;
-global using Masa.Stack.Components.GlobalNavigation;
+global using Microsoft.AspNetCore.Components;
+global using Microsoft.AspNetCore.Components.Web;
+global using Microsoft.Extensions.DependencyInjection;
