e5286dc270b70201f60c7e2dc36879a559a6391d
capdiem <capdiem@live.com>
2022-05-11 17:38:52 +0800

    🚧 feat: WIP

---
+++ src/Masa.Stack.Components/Models/User.cs
@@ -0,0 +1,50 @@
+﻿namespace Masa.Stack.Components.Models;
+
+public class User
+{
+    public string Account { get; set; }
+    
+    public string? Avatar { get; set; }
+    
+    public string DisplayName { get; set; }
+    
+    public string? Name { get; set; }
+    
+    /// <summary>
+    /// 性别 TODO: use enum?
+    /// </summary>
+    public int Gender { get; set; }
+    
+    /// <summary>
+    /// 工号
+    /// </summary>
+    public string? JobNumber { get; set; }
+
+    public string? Email { get; set; }
+
+    public string? Position { get; set; }
+    
+    public string? PhoneNumber { get; set; }
+    
+    public IEnumerable<string>? Teams { get; set; }
+    
+    public string? CompanyName { get; set; }
+    
+    /// <summary>
+    /// 国家或地区
+    /// </summary>
+    public string? Region { get; set; }
+
+    // TODO: 地址树
+    public string? Address { get; set; }
+    
+    // TODO: 部门树
+    public string? Department { get; set; }
+    
+    /// <summary>
+    /// 入职日期
+    /// </summary>
+    public DateTime? JoinedAt { get; set; }
+
+    public string GenderText => Gender == 0 ? "Female" : "Male";
+}
---
+++ src/Masa.Stack.Components/PureComponents/Defaults/DefaultTextfield.cs
@@ -0,0 +1,12 @@
+﻿namespace Masa.Stack.Components;
+
+public class DefaultTextfield<TValue> : MTextField<TValue>
+{
+    public override async Task SetParametersAsync(ParameterView parameters)
+    {
+        await base.SetParametersAsync(parameters);
+
+        HideDetails = "auto";
+        Outlined = true;
+    }
+}
