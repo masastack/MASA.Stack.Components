0d2d7c897537037fb98700adbeb2b45229bbc4e5
capdiem <capdiem@live.com>
2022-06-10 16:11:51 +0800

    ♻ refactor: expose NavItems parameter

--- src/Masa.Stack.Components/Layouts/Layout.razor.cs
+++ src/Masa.Stack.Components/Layouts/Layout.razor.cs
@@ -4,10 +4,7 @@ namespace Masa.Stack.Components;
 
 public partial class Layout
 {
-    /// <summary>
-    /// @Body
-    /// </summary>
-    [Parameter, EditorRequired]
+    [Parameter]
     public RenderFragment? ChildContent { get; set; }
 
     [Parameter, EditorRequired]
@@ -19,24 +16,16 @@ public partial class Layout
     [Parameter, EditorRequired]
     public string? MiniLogo { get; set; }
 
-    protected override async Task OnAfterRenderAsync(bool firstRender)
+    [Parameter, EditorRequired]
+    public List<Nav>? NavItems { get; set; }
+
+    protected override void OnParametersSet()
     {
-        if (!Navs.Any())
-        {
-            Navs = await FetchSystemNavs();
+        base.OnParametersSet();
 
-            FlattenedNavs = FlattenNavs(Navs);
-
-            StateHasChanged();
-        }
+        NavItems ??= new List<Nav>();
+        FlattenedNavs = FlattenNavs(NavItems);
     }
-
-    private List<Nav> Navs { get; set; } = new();
-
-    private List<PageTabItem> PageTabItems =>
-        FlattenedNavs.Where(n => n.Url is not null)
-                     .Select(nav => new PageTabItem(nav.Name, nav.Url, nav.Icon ?? ""))
-                     .ToList();
 
     private List<Nav> FlattenedNavs { get; set; } = new();
 
@@ -51,31 +40,5 @@ public partial class Layout
         }
 
         return res;
-    }
-
-    private async Task<List<Nav>> FetchSystemNavs()
-    {
-        return new List<Nav>()
-        {
-            new Nav("dashboard", "Dashboard", "mdi-view-dashboard-outline", "/", 1),
-            new Nav("counter", "Counter", "mdi-pencil", "/counter", 1),
-            new Nav("fetchdata", "Fetch data", "mdi-delete", "/fetchdata", 1),
-            new Nav("father", "Father", "mdi-numeric-0-box-outline", 1, new List<Nav>
-            {
-                new Nav("children2", "ChildTwo", 2, "father", new List<Nav>()
-                {
-                    new Nav("children", "ChildOne", "/has-children", 3, "children2"),
-                }),
-                new Nav("dialog", "dialog", "/dialog", 2, "father"),
-                new Nav("tab", "tab", "/tab", 2, "father"),
-                new Nav("mini", "mini", "/mini-components", 2, "father"),
-                new Nav("extend", "extend", "/extend", 2, "father"),
-                new Nav("userAutoCompleteExample", "userAutoComplete", "/userAutoCompleteExample", 2, "father"),
-                new Nav("defaultButtonExample", "defaultButton", "/defaultButtonExample", 2, "father"),
-                new Nav("defaultDataTableExample", "defaultDataTable", "/defaultDataTableExample", 2, "father"),
-                new Nav("paginationExample", "pagination", "/defaultPaginationExample", 2, "father"),
-                new Nav("uploadImageExample", "uploadImage", "/uploadImageExample", 2, "father")
-            }),
-        };
     }
 }
