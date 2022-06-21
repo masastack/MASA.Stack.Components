6d0e8c1031a118526481580b9d2d2d919ba8d9ca
wuweilai <824255785@qq.com>
2022-06-21 16:29:53 +0800

    feat(component):add PaginationSelect Component

---
+++ src/Masa.Stack.Components/IntegrationComponents/PaginationSelect.razor.cs
@@ -0,0 +1,32 @@
+﻿namespace Masa.Stack.Components;
+
+public partial class PaginationSelect
+{
+    [Parameter]
+    public int Value { get; set; }
+
+    [Parameter]
+    public EventCallback<int> ValueChanged { get; set; }
+
+    [Parameter]
+    public List<int> Items { get; set; } = new();
+
+    private string Icon { get; set; } = "mdi-menu-down";
+
+    public async Task SelectAsync(int value)
+    {
+        Value = value;
+        if (ValueChanged.HasDelegate)
+        {
+            await ValueChanged.InvokeAsync(value);
+        }
+        Icon = "mdi-menu-down";
+    }
+
+    public void ChangeIconState(bool show)
+    {
+        if (show) Icon = "mdi-menu-up";
+        else Icon = "mdi-menu-down";
+    }
+}
+
