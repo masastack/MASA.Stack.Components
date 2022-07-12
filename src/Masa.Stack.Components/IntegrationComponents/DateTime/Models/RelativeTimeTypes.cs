302c9852cbaaed538971b9b2ce9fd00ad1e37dd5
wuweilai <30889371+15168440402@users.noreply.github.com>
2022-07-12 18:43:35 +0800

    feature(datetime):add DatePicker、 DateRangePicker、 DateTimePicker、DateTimeRangePicker、RelativeTime (#98)
    * feat(dateTimePicker): add DateTimePicker 、DateTimeRangePicker
    
    * feat(dateTimePicker):dateTimePicker
    
    * feat(component)：add defaultpassword、defaultSheetDialog、copyPassword
    
    * feat(datimepicker):dateTimePicker
    
    * feat(dateTimePicker): dateTimePicker
    
    * recator:update nuget packge
    
    * fix

---
+++ src/Masa.Stack.Components/IntegrationComponents/DateTime/Models/RelativeTimeTypes.cs
@@ -0,0 +1,14 @@
+﻿namespace Masa.Stack.Components;
+
+public enum RelativeTimeTypes
+{
+    FifteenMinutes = 1,
+    ThirtyMinutes,
+    OneHour,
+    TwoHour,
+    TwelveHour,
+    OneDay,
+    OneWeek,
+    OneMonth
+}
+
