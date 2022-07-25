c0f51f675fa2b6d7b05666eb04ee4011dd5deeec
fengzhonghao <fengzhonghao@masastack.com>
2022-07-25 14:05:53 +0800

    feat:update default component setting

---
+++ src/Masa.Stack.Components/IntegrationComponents/Defaults/DefaultAutoComplete.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Masa.Stack.Components.IntegrationComponents.Defaults
+{
+    public class DefaultAutoComplete<TItem, TItemValue, TValue> : MAutocomplete<TItem, TItemValue, TValue>
+    {
+        [Parameter]
+        public bool Required { get; set; }
+
+        public override async Task SetParametersAsync(ParameterView parameters)
+        {
+            await base.SetParametersAsync(parameters);
+
+            Dense = true;
+            HideDetails = true;
+            Outlined = true;
+        }
+
+        protected override void OnParametersSet()
+        {
+            base.OnParametersSet();
+
+            if (Required && PrependInnerContent == default)
+            {
+                PrependInnerContent = builder =>
+                {
+                    builder.OpenElement(0, "label");
+                    builder.AddAttribute(1, "class", "red--text");
+                    builder.AddContent(2, "*");
+                    builder.CloseElement();
+                };
+            }
+        }
+    }
+}
