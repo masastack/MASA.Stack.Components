0d736fe719d0727c8ea8710a9b66e536c5c66526
wzh425 <wuzihao425@outlook.com>
2022-07-27 14:37:06 +0800

    temp

---
+++ src/Masa.Stack.Components/IntegrationComponents/SSimpleModal.cs
@@ -0,0 +1,14 @@
+﻿namespace Masa.Stack.Components.IntegrationComponents;
+
+public class SSimpleModal : SModal
+{
+    protected override ModalButtonProps GetDefaultCancelButtonProps()
+    {
+        var props = base.GetDefaultCancelButtonProps();
+        props.Text = false;
+        props.Rounded = true;
+        props.Style = "width:100px";
+
+        return props;
+    }
+}
