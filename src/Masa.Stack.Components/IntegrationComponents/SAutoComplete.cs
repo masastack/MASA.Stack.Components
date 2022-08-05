b38f9c3691389b1a2499d20e9aebee142d9f462b
wuweilai <824255785@qq.com>
2022-08-05 14:10:15 +0800

    feat(ui):optimization ui

--- src/Masa.Stack.Components/IntegrationComponents/SAutoComplete.cs
+++ src/Masa.Stack.Components/IntegrationComponents/SAutoComplete.cs
@@ -3,20 +3,62 @@
     public class SAutoComplete<TItem, TItemValue, TValue> : MAutocomplete<TItem, TItemValue, TValue>
     {
         [Parameter]
+        public bool Small { get; set; }
+
+        [Parameter]
+        public bool Medium { get; set; }
+
+        [Parameter]
+        public bool Large { get; set; }
+
+        [Parameter]
         public bool Required { get; set; }
 
         public override async Task SetParametersAsync(ParameterView parameters)
         {
-            await base.SetParametersAsync(parameters);
-
             Dense = true;
-            HideDetails = true;
+            HideDetails = "auto";
             Outlined = true;
+            HideSelected = true;
+            Color = "primary";
+            Style = "";
+            Class = "";
+            await base.SetParametersAsync(parameters);
         }
 
         protected override void OnParametersSet()
         {
             base.OnParametersSet();
+
+            Class ??= "";
+            if (Large is false && Small is false) Medium = true;
+            if (Dense is true)
+            {
+                if (Large)
+                {
+                    Height = 56;
+                    if (Class.Contains("m-input--dense-56") is false)
+                    {
+                        Class += " m-input--dense-56";
+                    }
+                }
+                else if (Medium)
+                {
+                    Height = 48;
+                    if (Class.Contains("m-input--dense-48") is false)
+                    {
+                        Class += " m-input--dense-48";
+                    }
+                }
+                else if (Small)
+                {
+                    Height = 40;
+                    if (Class.Contains("m-input--dense-40") is false)
+                    {
+                        Class += " m-input--dense-40";
+                    }
+                }
+            }
 
             if (Required && PrependInnerContent == default)
             {
