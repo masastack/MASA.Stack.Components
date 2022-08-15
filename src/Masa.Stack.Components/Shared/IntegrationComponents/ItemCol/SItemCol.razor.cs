3f18572b55766961d470630c789e44de03189cc4
capdiem <capdiem@live.com>
2022-08-15 14:01:51 +0800

    🎨 style: format code

--- src/Masa.Stack.Components/Shared/IntegrationComponents/ItemCol/SItemCol.razor.cs
+++ src/Masa.Stack.Components/Shared/IntegrationComponents/ItemCol/SItemCol.razor.cs
@@ -2,23 +2,32 @@
 
 public partial class SItemCol : MasaComponentBase
 {
-    [Parameter] public Func<bool, string>? BoolFormatter { get; set; }
+    [Parameter]
+    public Func<bool, string>? BoolFormatter { get; set; }
 
-    [Parameter] public bool ChippedEnum { get; set; }
+    [Parameter]
+    public bool ChippedEnum { get; set; }
 
-    [Parameter] public bool SmallChip { get; set; }
+    [Parameter]
+    public bool SmallChip { get; set; }
 
-    [Parameter] public Func<Enum, string>? EnumColorFormatter { get; set; }
+    [Parameter]
+    public Func<Enum, string>? EnumColorFormatter { get; set; }
 
-    [Parameter] public Func<DateTime, bool>? DateTimeValidator { get; set; }
+    [Parameter]
+    public Func<DateTime, bool>? DateTimeValidator { get; set; }
 
-    [Parameter] public bool IgnoreTime { get; set; }
+    [Parameter]
+    public bool IgnoreTime { get; set; }
 
-    [Parameter] public string? DateFormat { get; set; }
+    [Parameter]
+    public string? DateFormat { get; set; }
 
-    [Parameter] public string? TimeFormat { get; set; }
+    [Parameter]
+    public string? TimeFormat { get; set; }
 
-    [Parameter, EditorRequired] public object? Value { get; set; }
+    [Parameter, EditorRequired]
+    public object? Value { get; set; }
 
     protected object InternalValue { get; set; } = null!;
 
