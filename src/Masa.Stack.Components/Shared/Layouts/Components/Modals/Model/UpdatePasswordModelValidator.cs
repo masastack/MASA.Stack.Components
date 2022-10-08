1b679bc80c9a7045ed27543d893b5902a5b8a9c7
wuweilai <824255785@qq.com>
2022-10-08 17:27:07 +0800

    refactor code

--- src/Masa.Stack.Components/Shared/Layouts/Components/Modals/Model/UpdatePasswordModelValidator.cs
+++ src/Masa.Stack.Components/Shared/Layouts/Components/Modals/Model/UpdatePasswordModelValidator.cs
@@ -13,14 +13,14 @@ public class UpdatePasswordModelValidator : AbstractValidator<UpdatePasswordMode
             .NotEmpty()
             .Matches(@"^\S*(?=\S{7,})(?=\S*\d)(?=\S*[A-Za-z])\S*$")
             .WithMessage(i18N.T("PasswordFormatVerificationPrompt"))
+            .WithName(i18N.T("NewPassword"))
+            .NotEqual(u => u.OldPassword)
+            .WithMessage(i18N.T("EqualOldAndNewPassword"))
             .WithName(i18N.T("NewPassword"));
 
         RuleFor(m => m.ConfirmNewPassword)
             .NotEmpty()
             .Equal(u => u.NewPassword).WithMessage(i18N.T("FailToConfirmNewPassword"))
             .WithName(i18N.T("ConfirmNewPassword"));
-        RuleFor(m => m.NewPassword)
-            .NotEqual(u => u.OldPassword).WithMessage(i18N.T("EqualOldAndNewPassword"))
-            .WithName(i18N.T("NewPassword"));
     }
 }
