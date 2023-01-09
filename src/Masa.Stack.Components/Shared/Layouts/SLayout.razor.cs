d98b6dde5bb095423198ef5fcc0bf0748d618c9f
wuweilai <30889371+15168440402@users.noreply.github.com>
2023-01-09 13:08:12 +0800

    Fix(expansionAppItem): expansion app item (#303)
    * fix ExpansionAppItem
    
    * fix route bug
    
    * refactor code

--- src/Masa.Stack.Components/Shared/Layouts/SLayout.razor.cs
+++ src/Masa.Stack.Components/Shared/Layouts/SLayout.razor.cs
@@ -132,7 +132,7 @@ public partial class SLayout
                 return;
             }
             var relativeUri = NavigationManager.Uri.Replace(NavigationManager.BaseUri, "");
-            if (!IsMenusUri(NavItems, relativeUri))
+            if (relativeUri.Contains("dashboard") is false && !IsMenusUri(NavItems, relativeUri))
             {
                 NavigationManager.NavigateTo("/403");
                 return;
