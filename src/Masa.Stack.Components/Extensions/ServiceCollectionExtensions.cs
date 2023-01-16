71b20fbadc43e69e651fb7fbbe572f58ba6e2024
MayueCif <6133287+MayueCif@users.noreply.github.com>
2023-01-16 12:25:15 +0800

    Feat/update package (#320)
    * feat:update framewokr
    
    * feat:update package
    
    * feat:update es port
    
    Co-authored-by: Mayue <mayue@lonsid.cn>

--- src/Masa.Stack.Components/Extensions/ServiceCollectionExtensions.cs
+++ src/Masa.Stack.Components/Extensions/ServiceCollectionExtensions.cs
@@ -41,7 +41,7 @@ public static class ServiceCollectionExtensions
                     new RedisServerOptions()
                     {
                         Host= masaStackConfig.RedisModel.RedisHost,
-                        Port=   masaStackConfig.RedisModel.RedisPort
+                        Port= masaStackConfig.RedisModel.RedisPort
                     }
                 },
                 DefaultDatabase = masaStackConfig.RedisModel.RedisDb,
@@ -63,13 +63,9 @@ public static class ServiceCollectionExtensions
                 return new UserAutoCompleteOptions
                 {
                     Index = masaStackConfig.ElasticModel.Index,
-                    Nodes = new string[1] { masaStackConfig.ElasticModel.ESNode }
+                    Nodes = new string[1] { $"{masaStackConfig.ElasticModel.ESNode}:{masaStackConfig.ElasticModel.ESPort}" }
                 };
             });
-
-            var autoCompleteFactory = builder.Services.BuildServiceProvider().GetRequiredService<IAutoCompleteFactory>();
-            var autoCompleteClient = autoCompleteFactory.Create();
-            autoCompleteClient.BuildAsync();
         }
         else
         {
