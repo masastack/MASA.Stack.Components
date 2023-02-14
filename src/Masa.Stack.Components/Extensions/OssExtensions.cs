8aa17021505a1db1b39378b3e19711bcadbc76e9
wuweilai <824255785@qq.com>
2023-02-14 17:49:30 +0800

    feat:update packge 0214

--- src/Masa.Stack.Components/Extensions/OssExtensions.cs
+++ src/Masa.Stack.Components/Extensions/OssExtensions.cs
@@ -1,23 +1,25 @@
 ﻿// Copyright (c) MASA Stack All rights reserved.
 // Licensed under the Apache License. See LICENSE.txt in the project root for license information.
 
+using Masa.Contrib.Storage.ObjectStorage.Aliyun;
+using Masa.Stack.Components.UserCenters.Models;
+
 namespace Masa.Stack.Components.Extensions;
 
 public static class OssExtensions
 {
     public static void AddOss(this IServiceCollection services)
     {
-        services.AddAliyunStorage((provider) =>
+        services.AddObjectStorage(option =>
         {
-            var ossOptions = provider.GetRequiredService<IMasaConfiguration>().ConfigurationApi.GetPublic()
-                    .GetSection("$public.OSS").Get<OssOptions>();
-            return new AliyunStorageOptions(ossOptions.AccessId, ossOptions.AccessSecret, ossOptions.Endpoint, ossOptions.RoleArn, ossOptions.RoleSessionName)
+            var ossOptions = services.GetMasaConfiguration().ConfigurationApi.GetPublic().GetSection("$public.OSS").Get<OssOptions>();
+            option.UseAliyunStorage(new AliyunStorageOptions(ossOptions.AccessId, ossOptions.AccessSecret, ossOptions.Endpoint, ossOptions.RoleArn, ossOptions.RoleSessionName)
             {
                 Sts = new AliyunStsOptions()
                 {
                     RegionId = ossOptions.RegionId
                 }
-            };
+            });
         });
     }
 }
