0a212ae217034e6d16dbaba9f9507b56b0d169b7
MayueCif <6133287+MayueCif@users.noreply.github.com>
2023-03-14 15:22:50 +0800

    Feat/logout bc (#399)
    * feat:add logout session
    
    * feat:udpate package
    
    * feat:add back channel
    
    ---------
    
    Co-authored-by: Mayue <mayue@lonsid.cn>

---
+++ src/Masa.Stack.Components/Extensions/OpenIdConnect/LogoutSessionManager.cs
@@ -0,0 +1,62 @@
+﻿namespace Masa.Stack.Components.Extensions.OpenIdConnect;
+
+public class LogoutSessionManager
+{
+    private static readonly object _lock = new object();
+    private readonly ILogger<LogoutSessionManager> _logger;
+    readonly IDistributedCacheClient _distributedCacheClient;
+
+
+    public LogoutSessionManager(ILoggerFactory loggerFactory, IDistributedCacheClient distributedCacheClient)
+    {
+        _distributedCacheClient = distributedCacheClient;
+        _logger = loggerFactory.CreateLogger<LogoutSessionManager>();
+    }
+
+    public void Add(string sub, string sid)
+    {
+        _logger.LogDebug($"Backchannel add a logout to the session: sub: {sub}, sid: {sid}");
+        //todo Masa DistributedLock
+        lock (_lock)
+        {
+            var key = sub + sid;
+            var logoutSession = new BackchannelLogoutSession(sub, sid);
+            _logger.LogDebug($"Backchannel logoutSession: {logoutSession}");
+            _distributedCacheClient.Set(key, logoutSession, TimeSpan.FromDays(7));
+        }
+    }
+
+    public async Task<bool> IsLoggedOutAsync(string sub, string sid)
+    {
+        _logger.LogDebug($"Backchannel IsLoggedOutAsync: sub: {sub}, sid: {sid}");
+        var key = sub + sid;
+        var matches = false;
+        var logoutSession = await _distributedCacheClient.GetAsync<BackchannelLogoutSession>(key);
+        if (logoutSession != null)
+        {
+            matches = logoutSession.IsMatch(sub, sid);
+            _logger.LogDebug($"Backchannel Logout session exists T/F {matches} : {sub}, sid: {sid}");
+        }
+        return matches;
+    }
+}
+
+class BackchannelLogoutSession
+{
+    public string Sub { get; set; }
+
+    public string Sid { get; set; }
+
+    public BackchannelLogoutSession(string sub, string sid)
+    {
+        Sub = sub;
+        Sid = sid;
+    }
+
+    public bool IsMatch(string sub, string sid)
+    {
+        return Sid == sid && Sub == sub ||
+               Sid == sid && Sub == null ||
+               Sid == null && Sub == sub;
+    }
+}
