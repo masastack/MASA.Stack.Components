596ef674efcf82b591d078a1eb19757a6624da75
Kogel <935368322@qq.com>
2023-03-20 19:28:43 +0800

    fix:update userinforazor

---
+++ src/Masa.Stack.Components/Pages/UserCenters/Modals/UpdateNickNameModal.razor.cs
@@ -0,0 +1,34 @@
+﻿namespace Masa.Stack.Components.UserCenters;
+
+public partial class UpdateNickNameModal : MasaComponentBase
+{
+    [Parameter]
+    public bool Visible { get; set; }
+
+    [Parameter]
+    public EventCallback<bool> VisibleChanged { get; set; }
+
+    [Parameter]
+    public EventCallback<string> OnSuccess { get; set; }
+
+    public MForm FormRef { get; set; } = default!;
+
+   // public UpdateUserNickNameModel UpdateUserNickName { get; set; } = new();
+    private async Task HandleOnOk()
+    {
+        if (FormRef.Validate())
+        {
+
+        }
+    }
+
+    private async Task HandleOnCancel()
+    {
+        FormRef.Reset();
+        if (VisibleChanged.HasDelegate)
+            await VisibleChanged.InvokeAsync(false);
+        else Visible = false;
+    }
+
+}
+
---
+++ src/Masa.Stack.Components/Pages/UserCenters/Models/UpdateUserNickNameModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Masa.Stack.Components.UserCenters.Models;
+public class UpdateUserNickNameModel
+{
+    public string NickName { get; set; }
+
+    public UpdateUserNickNameModel(string nickName)
+    {
+        NickName = nickName;
+    }
+}
+
