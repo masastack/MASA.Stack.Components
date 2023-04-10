44beec3ffb5f31ddd0c76a7280186495bd769544
zhendong.dong <benguahao@foxmail.com>
2023-04-10 09:40:00 +0800

    fix: remove unused file
    This is an out-of-date document that is no longer needed

--- src/Masa.Stack.Components/wwwroot/Masa.Stack.Components.lib.module.js
+++
@@ -1,7 +0,0 @@
-﻿export function beforeStart(options, extensions) {
-}
-
-export function afterStarted(blazor) {
-  const offset = new Date().getTimezoneOffset();
-  DotNet.invokeMethodAsync("Masa.Stack.Components", "SetTimezoneOffset", offset)
-}
