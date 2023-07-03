ea2c760c193e51372f876da8375f9eec22b80354
Mayue <mayue8373@live.com>
2023-07-03 14:21:11 +0800

    feat:update mf

--- tests/MasaWebApp/Program.cs
+++ tests/MasaWebApp/Program.cs
@@ -23,7 +23,7 @@ builder.Services.AddMasaOpenIdConnect(new MasaOpenIdConnectOptions
     Scopes = new List<string> { "offline_access" }
 });
 
-await builder.Services.AddMasaStackComponentsAsync(MasaStackProject.Auth);
+await builder.Services.AddMasaStackComponentsAsync(new MasaStackProject(6, "test"));
 
 var app = builder.Build();
 
