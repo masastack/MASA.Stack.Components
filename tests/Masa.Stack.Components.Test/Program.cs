2b5fc360796e9782f442b9fbd790c858f5a175ef
duia <532977252@qq.com>
2023-11-13 10:29:29 +0800

    feat:init

---
+++ tests/Masa.Stack.Components.Test/Program.cs
@@ -0,0 +1,46 @@
+using Masa.Contrib.StackSdks.Caller;
+using Masa.Contrib.StackSdks.Config;
+using Masa.Stack.Components.Extensions.OpenIdConnect;
+using Masa.Stack.Components.Test.Data;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+var builder = WebApplication.CreateBuilder(args);
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddScoped<TokenProvider>();
+
+await builder.Services.AddMasaStackConfigAsync(MasaStackProject.Auth, MasaStackApp.WEB);
+var masaStackConfig = builder.Services.GetMasaStackConfig();
+builder.Services.AddMasaOpenIdConnect(new MasaOpenIdConnectOptions
+{
+    Authority = masaStackConfig.GetSsoDomain(),
+    ClientId = masaStackConfig.GetWebId(MasaStackProject.Auth),
+    Scopes = new List<string> { "offline_access" }
+});
+
+//await builder.Services.AddMasaStackComponentsAsync(new MasaStackProject(6, "test"));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
--- tests/MasaWasmApp/Program.cs
+++ tests/MasaWasmApp/Program.cs
@@ -5,6 +5,11 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+//DccOptions dccOptions = builder.Configuration.GetSection("DccOptions").Get<DccOptions>();
+builder.Services.AddMasaConfiguration(configurationBuilder =>
+{
+    configurationBuilder.UseDcc();//使用Dcc 扩展Configuration能力，支持远程配置
+});
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
