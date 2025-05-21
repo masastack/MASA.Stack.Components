2f24835345652d9f24dca5dbc77377b056b04a84
MayueCif <mayue8373@live.com>
2025-05-21 10:50:53 +0800

    chore: update Masa.Blazor version and remove TokenEndpoint (#759)
    Updated the `Masa.Blazor` package version from `1.9.0` to `1.9.3` in the `Masa.Stack.Components.Standalone.csproj` file.
    Removed the `TokenEndpoint` class along with its properties `AccessToken` and `RefreshToken` from `TokenEndpoint.cs`.

--- src/Masa.Stack.Components/Extensions/OpenIdConnect/TokenEndpoint.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Masa.Stack.Components.Extensions.OpenIdConnect;
-
-public class TokenEndpoint
-{
-    public string AccessToken { get; set; } = string.Empty;
-
-    public string RefreshToken { get; set; } = string.Empty;
-}
