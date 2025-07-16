45b466ea6b068075de919da904410b355ce4d6f9
qinyouzeng <qinyouzeng@masastack.com>
2025-07-16 11:02:42 +0800

    feat: support select random file name when upload file to oss

--- src/Masa.Stack.Components/wwwroot/js/oss/upload.js
+++ src/Masa.Stack.Components/wwwroot/js/oss/upload.js
@@ -19,11 +19,12 @@ async function UploadImage(imageFiles, ossParamter) {
         // 指定CopyObject操作时是否覆盖同名目标Object。此处设置为true，表示禁止覆盖同名Object。
         // 'x-oss-forbid-overwrite': 'true',
     };
-    return await putObject(client, imageFiles[0], headers, cdnHost);
+    console.log(ossParamter);
+    return await putObject(client, imageFiles[0], headers, cdnHost, ossParamter.randomName);
 }
 
 function getCdnHost(ossParamter) {
-    let cdnHost = ((ossParamter || {}).cdnHost || "").trim();
+    let cdnHost = (ossParamter.cdnHost || "").trim();
     if (cdnHost.length == 0) {
         return cdnHost;
     }
@@ -31,20 +32,40 @@ function getCdnHost(ossParamter) {
         return cdnHost.substring(0, cdnHost.length - 1);
 }
 
-async function putObject(client, file, headers, cdnHost) {
+function getFileName(file, rand) {
+    if (!rand) return file.name;
+    let time = formatTime(new Date()), random = Math.random().toString().slice(3, 9);
+    let lastPosition = file.name.lastIndexOf('.');
+    let ext = lastPosition >= 0 ? file.name.slice(lastPosition) : '';
+    return `${time}_${random}${ext}`;
+}
+
+function formatTime(date) {
+    const year = date.getFullYear();
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const day = date.getDate().toString().padStart(2, '0');
+    const hour = date.getHours().toString().padStart(2, '0');
+    const minute = date.getMinutes().toString().padStart(2, '0');
+    const second = date.getSeconds().toString().padStart(2, '0');
+    const offset = -date.getTimezoneOffset();
+    const sign = offset >= 0 ? 1 : 0;
+    return `${year}${month}${day}${hour}${minute}${second}_${sign}${offset}`;
+}
+
+async function putObject(client, file, headers, cdnHost, randName) {
     try {
+        let fileName = getFileName(file, randName);
         // 填写Object完整路径。Object完整路径中不能包含Bucket名称。
         // 您可以通过自定义文件名（例如exampleobject.txt）或文件完整路径（例如exampledir/exampleobject.txt）的形式实现将数据上传到当前Bucket或Bucket中的指定目录。
         // data对象可以自定义为file对象、Blob数据或者OSS Buffer。
         const result = await client.put(
-            `${file.name}`,
+            `${fileName}`,
             file,
             {
                 headers
             }
         );
         console.log(result);
-        console.log("cdnHost: " + cdnHost);
         let url = cdnHost ? (cdnHost + "/" + result.name) : result.url;
         console.log(url)
         return [url];
