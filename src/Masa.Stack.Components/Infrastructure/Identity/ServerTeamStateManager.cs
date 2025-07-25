7b90af6ee2899e45de5fe2baada23274e1d6d2df
MayueCif <mayue8373@live.com>
2025-07-25 17:11:20 +0800

    Feat/update state manager (#778)
    * feat(TeamStateManager): add support for WASM and Server modes
    
    - Implemented ITeamStateManager interface for managing team states.
    - Added registration logic for team state manager in ServiceCollectionExtensions.cs.
    - Created ServerTeamStateManager and WasmTeamStateManager to handle team state management in respective environments.
    - Updated User.razor to utilize the new team state manager for fetching user team information.
    
    * feat:update WasmTeamStateManager refresh token
    
    * fix:navigate page
    
    * fix(WasmTeamStateManager): update token refresh logging
    
    Updated the log message for token refresh failure and removed the null check for newToken. The logic now directly navigates to the page refresh on token refresh failure, simplifying the code.
    
    * feat: Remove team state retrieval methods
    
    Removed the methods for retrieving and clearing the current team ID from the ITeamStateManager interface and its implementations in ServerTeamStateManager and WasmTeamStateManager. The User.razor file was updated to directly use MasaUser.CurrentTeamId instead of relying on the authentication state for the current team ID.

--- src/Masa.Stack.Components/Infrastructure/Identity/ServerTeamStateManager.cs
+++ src/Masa.Stack.Components/Infrastructure/Identity/ServerTeamStateManager.cs
@@ -24,28 +24,4 @@ public class ServerTeamStateManager : ITeamStateManager, IScopedDependency
     {
         await _authenticationStateManager.UpsertClaimAsync(IdentityClaimConsts.CURRENT_TEAM, teamId.ToString());
     }
-
-    /// <summary>
-    /// 从身份验证状态中获取当前团队ID
-    /// </summary>
-    public async Task<Guid> GetCurrentTeamAsync()
-    {
-        var authState = await _authenticationStateProvider.GetAuthenticationStateAsync();
-        var teamIdClaim = authState.User.FindFirst(IdentityClaimConsts.CURRENT_TEAM);
-        
-        if (teamIdClaim != null && Guid.TryParse(teamIdClaim.Value, out var teamId))
-        {
-            return teamId;
-        }
-
-        return Guid.Empty;
-    }
-
-    /// <summary>
-    /// 清除团队状态
-    /// </summary>
-    public async Task ClearTeamStateAsync()
-    {
-        await _authenticationStateManager.UpsertClaimAsync(IdentityClaimConsts.CURRENT_TEAM, Guid.Empty.ToString());
-    }
-} 
+}
